6ebf50da953ded22b87d1aa712d0f9b291162529
nguyennguyen <nguyen_ng98@yahoo.com>
2019-05-23 16:17:45 -0700

    eslint problem

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "../Navigation/navigation";
 import LandingPage from "../Landing/landing";
@@ -7,19 +7,19 @@ import PasswordForgetPage from "../PasswordForget/pwForget";
 import AdminPanelPage from "../Admin/admin";
 import * as ROUTES from "../../constants/routes";
 
-function App () {
-    return (
-      <Router>
-        <div>
-          <Navigation />
-          <hr />
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-          <Route path={ROUTES.ADMIN_PANEL} component={AdminPanelPage} />
-        </div>
-      </Router>
-    );
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <hr />
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.ADMIN_PANEL} component={AdminPanelPage} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
