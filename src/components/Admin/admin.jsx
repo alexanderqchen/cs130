d05e148ff26474cc3822ee85350506e5dffcc711
Jason Wong <jf126dolphin@gmail.com>
2019-05-27 13:35:29 -0700

    Put the Invite Admin workflow on the admin page, for development purposes. Will need to move the feature to the settings page later

--- src/components/Admin/admin.jsx
+++ src/components/Admin/admin.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import withAuthorization from "../Session/withAuthorization";
+import InviteAdminButtom from "../InviteAdmin/inviteAdmin";
+import SignOutButton from "../SignOut/signOut";
 
 function Admin() {
-  return <div>Admin Page</div>;
+  // Note(jason): We are putting the InviteAdmin features here for
+  // development. We will need to move it to the settings page later.
+  return (
+    <div>
+      <InviteAdminButtom />
+      <SignOutButton />
+      Admin Page
+    </div>
+  );
 }
 
 export default withAuthorization(Admin);
