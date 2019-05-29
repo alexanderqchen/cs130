dc999be231bf4f1fb6ca12275a3aa839d4b2f98f
Jason Wong <jf126dolphin@gmail.com>
2019-05-28 17:48:23 -0700

    In Settings page, pull email addresses from backend, and add an enable switch

--- src/components/Settings/settings.jsx
+++ src/components/Settings/settings.jsx
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
-import DeleteIcon from "@material-ui/icons/Close";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Fab from "@material-ui/core/Fab";
 import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
 import InputLabel from "@material-ui/core/InputLabel";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import TextField from "@material-ui/core/TextField";
+import { Switch } from "@material-ui/core";
 import PropTypes from "prop-types";
+import Firebase, { withFirebase } from "../Firebase/firebase";
 import "typeface-roboto";
 
 // TODOs:
@@ -60,7 +60,8 @@ const styles = {
 class Settings extends Component {
   static get propTypes() {
     return {
-      classes: PropTypes.instanceOf(Object).isRequired
+      classes: PropTypes.instanceOf(Object).isRequired,
+      firebase: PropTypes.instanceOf(Firebase).isRequired
     };
   }
 
@@ -68,24 +69,58 @@ class Settings extends Component {
     super(props);
     this.state = {
       openAdd: false,
-      openDelete: false
+      openDelete: false,
+      emailInput: "",
+      emailInputWellFormatted: false,
+      loading: true,
+      users: null,
+      usersObject: null
     };
-    this.generate = this.generate.bind(this);
+    this.onEmailInputChange = this.onEmailInputChange.bind(this);
+
     this.handleClickOpenAdd = this.handleClickOpenAdd.bind(this);
     this.handleCloseAdd = this.handleCloseAdd.bind(this);
     this.handleConfirmAdd = this.handleConfirmAdd.bind(this);
     this.handleClickOpenDelete = this.handleClickOpenDelete.bind(this);
     this.handleCloseDelete = this.handleCloseDelete.bind(this);
     this.handleConfirmDelete = this.handleConfirmDelete.bind(this);
+
+    this.fetchUserList = this.fetchUserList.bind(this);
+    this.doToggleEnabled = this.doToggleEnabled.bind(this);
+    this.writeUserToDb = this.writeUserToDb.bind(this);
   }
 
-  // Used to fetch a list of valid email addresses
-  generate(element) {
-    this.x = 1; // to bypass "use-this" lint error
-    return [0, 1, 2].map(value => {
-      return React.cloneElement(element, {
-        key: value
-      });
+  componentDidMount() {
+    const { firebase } = this.props;
+    this.setState({
+      loading: true
+    });
+
+    firebase.users().on("value", snapshot => {
+      const usersObject = snapshot.val();
+
+      if (usersObject) {
+        const usersList = Object.keys(usersObject);
+
+        this.setState({
+          users: usersList,
+          usersObject,
+          loading: false
+        });
+        return;
+      }
+
+      this.setState({ users: null, loading: false });
+    });
+  }
+
+  onEmailInputChange(event) {
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const email = event.target.value;
+
+    this.setState({
+      emailInput: email,
+      emailInputWellFormatted: regexEmail.test(email)
     });
   }
 
@@ -105,6 +140,8 @@ class Settings extends Component {
 
   // Handle the logic when a user adds an account (Presses Add in Add dialog)
   handleConfirmAdd() {
+    const { emailInput } = this.state;
+    this.writeUserToDb(emailInput, true);
     // Add logic here
     this.setState({
       openAdd: false
@@ -133,34 +170,65 @@ class Settings extends Component {
     });
   }
 
+  // Used to fetch a list of valid email addresses
+  fetchUserList() {
+    const { loading, users, usersObject } = this.state;
+    if (loading) {
+      return <div>Loading list of admins</div>;
+    }
+
+    if (!users) {
+      return <div>Unable to load list of admins</div>;
+    }
+
+    if (users.length === 0) {
+      return <div>No admins found</div>;
+    }
+
+    const usersListItems = users.map(email => {
+      return (
+        <div>
+          <hr />
+          <ListItem>
+            <ListItemText primary={email.replace(",", ".")} />
+            <ListItemSecondaryAction>
+              <Switch
+                checked={usersObject[email].enabled}
+                onChange={this.doToggleEnabled(email)}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        </div>
+      );
+    });
+
+    return <div>{usersListItems}</div>;
+  }
+
+  doToggleEnabled(email) {
+    return event => {
+      this.writeUserToDb(email, event.target.checked);
+    };
+  }
+
+  writeUserToDb(email, enabled) {
+    const { firebase } = this.props;
+
+    firebase.user(email.replace(".", ",")).set({
+      enabled
+    });
+  }
+
   render() {
     const { classes } = this.props;
-    const { openAdd, openDelete } = this.state;
+    const { openAdd, openDelete, emailInputWellFormatted } = this.state;
     return (
       <div className={classes.root}>
         <h1 className={classes.title}>Accounts</h1>
         <Grid container>
           <Grid item xs={4}>
             <div className={classes.emailList}>
-              <List>
-                {this.generate(
-                  <div>
-                    <hr />
-                    <ListItem>
-                      <ListItemText primary="some email here" />
-                      <ListItemSecondaryAction>
-                        <IconButton
-                          edge="end"
-                          aria-label="Delete"
-                          onClick={this.handleClickOpenDelete}
-                        >
-                          <DeleteIcon />
-                        </IconButton>
-                      </ListItemSecondaryAction>
-                    </ListItem>
-                  </div>
-                )}
-              </List>
+              <List>{this.fetchUserList()}</List>
             </div>
           </Grid>
           <Grid item xs={4} />
@@ -186,7 +254,11 @@ class Settings extends Component {
             </DialogTitle>
             <FormControl>
               <InputLabel shrink>Email</InputLabel>
-              <TextField margin="normal" variant="outlined" />
+              <TextField
+                margin="normal"
+                variant="outlined"
+                onChange={this.onEmailInputChange}
+              />
             </FormControl>
             <DialogActions>
               <Button
@@ -200,6 +272,7 @@ class Settings extends Component {
                 onClick={this.handleConfirmAdd}
                 color="primary"
                 classes={{ label: classes.confirmAdd }}
+                disabled={!emailInputWellFormatted}
               >
                 Add
               </Button>
@@ -240,4 +313,4 @@ class Settings extends Component {
   }
 }
 
-export default withStyles(styles)(Settings);
+export default withFirebase(withStyles(styles)(Settings));
